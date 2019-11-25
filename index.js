1ee29b39cd1d3702bb3235d44ce3148a273b9aaf
張皓偉 <otis8496@gmail.com>
2019-11-25 21:33:20 +0800

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#thanks1").hide();
+  $("#thanks2").hide();
+  $("#btn1").click(function(){
+    $("#thanks1").show();
+   });
+  $("#btn2").click(function(){
+    $("#thanks2").show();
+   });
+});
+  
