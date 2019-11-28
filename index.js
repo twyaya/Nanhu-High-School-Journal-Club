653bb19589d953832e7f24fd0aba17591025ac2a
張皓偉 <otis8496@gmail.com>
2019-11-28 18:58:35 +0800

    更新CSS

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ $(document).ready(function(){
   alert("建議用電腦觀看已達最佳效果");
   $("#thanks1").hide();
   $("#thanks2").hide();
+  $("#school2").hide();
+  $("#school3").hide();
+  
   $("#btn1").click(function(){
+    $("#school1").show();
+    $("#school2").hide();
+    $("#school3").hide();
+   });
+   
+  $("#btn2").click(function(){
+    $("#school1").hide();
+    $("#school2").show();
+    $("#school3").hide();
+   });
+   
+  $("#btn3").click(function(){
+    $("#school1").hide();
+    $("#school2").hide();
+    $("#school3").show();
+   });
+   
+  $("#btn4").click(function(){
     $("#thanks1").show();
    });
-  $("#btn2").click(function(){
+  $("#btn5").click(function(){
     $("#thanks2").show();
    });
 });
